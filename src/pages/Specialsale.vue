af8a0a0b8c5a9ad31cc66e34a7753d057d6ce57d
HirokiOmote <bubo@bubo.jp>
2017-02-16 18:20:38 +0900

    Add footer comment

--- src/pages/Specialsale.vue
+++ src/pages/Specialsale.vue
@@ -26,6 +26,11 @@
       <product v-for="product in products" :product="product"></product>
     </ul>
     <a class="wow bounceInLeft Btn" href="#" target="_blank">WEBチラシを見る<span>(PDF:00kb)</span></a>
+    <aside>
+      <p>【おことわり】<br>
+      ※台数限定商品は、特別商材につきお一人様１商品かつ１台限り、表示台数限りの先着販売となります。※ご来店多数の場合、先着順にてお一人様１枚限り整理券を配布させていただく場合がございます。※整理券は、お一人様１商品かつ１台限りとさせていただきます。異なる商品分を含め一枚のみとさせていただきます。※同業者及び転売目的の方への販売は固くお断りさせていただきます。また、｢同一名義によるご購入」及び「同一住所へのお届け」はお断りさせていただきます。※なくなり次第終了とさせていただきます。※配達・設置には別途送料が必要になる場合があります。※台数限定商品は他のセール値引き及び各種値引き券の利用はできません。※お電話でのご予約ご注文はお受けいたしかねます。 ※徹夜での並んでのお買い求めは固くお断りします。※日替り<br>
+      限定品に並ばれる際に、中学生未満のお客様は人数の対象外とさせていただきます。※順番待ちのトラブル等があった場合、販売を中止させていただく場合もございます。※荷物をおいての場所取りはご遠慮ください。やむを得ず場所を離れる場合は、前後の方に一声かけていただき、速やかにお戻りください。</p>
+    </aside>
     <footer>
       <p><small>Copyright (C) 2017 Y3Q Co.,Ltd,. All Rights Reserved. </small></p>
     </footer>
@@ -103,6 +108,15 @@ export default {
     background-color: #b7a014;
     @include media( md ) {
       font-size: 2.1rem;
+    }
+  }
+
+  aside {
+    margin: 1em 15px;
+    color: #fff;
+    line-height: 1.5;
+    @include media( sm ) {
+      display: none;
     }
   }
 
